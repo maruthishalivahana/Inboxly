964dc3429c8262439ebdc2efd7ad7e7bf1527868
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-08-17 15:36:39 +0530

    feat: Implemented authentication and connect the app to DB.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,36 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((error) => {
+    console.error("MongoDB connection error:", error);
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
