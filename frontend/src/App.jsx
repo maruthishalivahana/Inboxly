a437eaefc3865406b94038534ac5c3f965658a26
Jeevan9898 <jeevanyadav9898@gmail.com>
2025-08-20 18:43:11 +0530

    Added Profile Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-export default function App() {
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Profile from "./Components/Profile.jsx";
+
+
+
+function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-extrabold text-blue-600">
-        Inboxly – Messenger Platform 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
